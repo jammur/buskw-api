d9434196185c895bf773f7de5fdce35e471439fa
Jamie Murai <murai.jamie@gmail.com>
2011-04-18 17:15:45 -0400

    Add route APIs

--- app.rb
+++ app.rb
@@ -5,8 +5,19 @@ end
 get '/stops' do
   content_type :json
   
+  stops = Array.new
+  
+  Stop.all.each do |stop|
+    stops << {
+      id: stop.id,
+      name: stop.name,
+      latitude: stop.latitude,
+      longitude: stop.longitude
+    }
+  end
+  
   output = {
-    stops: Stop.all
+    stops: stops
   }
   
   output.to_json
@@ -15,14 +26,24 @@ end
 get '/stops/:stop_id' do
   content_type :json
   
-  output = Stop.find(params[:stop_id])
-  
-  if output.nil?
+  stop = Stop.find(params[:stop_id])
+  if stop.nil?
     search_error = {
       message: "Unable to find stop with id #{params[:stop_id]}"
     }
     output = {
       error: search_error
+    }
+  else
+    stopOutput = {
+      id: stop.id,
+      name: stop.name,
+      latitude: stop.latitude,
+      longitude: stop.longitude
+    }
+    
+    output = {
+      stop: stopOutput
     }
   end
   
@@ -43,8 +64,22 @@ get '/stops/:stop_id/times' do
     }
   else
     times = StopTime.where(stop_id: params[:stop_id])
+    
+    timesOutput = Array.new
+    
+    times.each do |time|
+      timesOutput << {
+        stop_id: time.stop_id,
+        trip_id: time.trip_id,
+        route_id: time.route_id,
+        arrival_time: time.arrival_time,
+        departure_time: time.departure_time,
+        stop_sequence: time.stop_sequence
+      }
+    end
+    
     output = {
-      stop_times: times
+      stop_times: timesOutput
     }
   end
   
@@ -74,8 +109,111 @@ get '/stops/:stop_id/times/:route_id' do
         error: search_error
       }
     else
+      timesOutput = Array.new
+
+      times.each do |time|
+        timesOutput << {
+          stop_id: time.stop_id,
+          trip_id: time.trip_id,
+          route_id: time.route_id,
+          arrival_time: time.arrival_time,
+          departure_time: time.departure_time,
+          stop_sequence: time.stop_sequence
+        }
+      end
+      
       output = {
-        stop_times: times
+        stop_times: timesOutput
+      }
+    end
+  end
+  
+  output.to_json
+end
+
+get '/routes' do
+  content_type :json
+  
+  routes = Array.new
+  
+  Route.all.each do |route|
+    routes << {
+      id: route.id,
+      name: route.name
+    }
+  end
+  
+  output = {
+    routes: routes
+  }
+  
+  output.to_json
+end
+
+get '/routes/:route_id' do
+  content_type :json
+  
+  route = Route.find(params[:route_id])
+  
+  if route.nil?
+    search_error = {
+      message: "Unable to find route with id #{params[:route_id]}"
+    }
+    output = {
+      error: search_error
+    }
+  else
+    routeOutput = {
+      id: route.id,
+      name: route.name
+    }
+    
+    output = {
+      route: routeOutput
+    }
+  end
+  
+  output.to_json
+end
+
+get '/routes/:route_id/times' do
+  content_type :json
+  
+  route = Route.find(params[:route_id])
+  
+  if route.nil?
+    search_error = {
+      message: "Unable to find route with id #{params[:route_id]}"
+    }
+    output = {
+      error: search_error
+    }
+  else
+    times = StopTime.where(route_id: params[:route_id]).asc(:trip_id)
+    
+    if times.nil?
+      search_error = {
+        message: "Route #{params[:route_id]} has no stops"
+      }
+      output = {
+        error: search_error
+      }
+    else
+      timesOutput = Array.new
+
+      times.each do |time|
+        timesOutput << {
+          stop_id: time.stop_id,
+          trip_id: time.trip_id,
+          route_id: time.route_id,
+          arrival_time: time.arrival_time,
+          departure_time: time.departure_time,
+          stop_sequence: time.stop_sequence
+        }
+      end
+      
+      output = {
+        stop_times: timesOutput
       }
     end
   end
