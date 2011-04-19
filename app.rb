3113d4032682b76d7b40dd22513a6dd090db5028
Jamie Murai <murai.jamie@gmail.com>
2011-04-18 23:46:35 -0400

    Refactor route methods and update API reference

--- app.rb
+++ app.rb
@@ -16,11 +16,7 @@ get '/stops' do
     }
   end
   
-  output = {
-    stops: stops
-  }
-  
-  output.to_json
+  stops.to_json
 end
 
 get '/stops/:stop_id' do
@@ -28,62 +24,44 @@ get '/stops/:stop_id' do
   
   stop = Stop.find(params[:stop_id])
   if stop.nil?
-    search_error = {
-      message: "Unable to find stop with id #{params[:stop_id]}"
-    }
-    output = {
-      error: search_error
-    }
-  else
-    stopOutput = {
-      id: stop.id,
-      name: stop.name,
-      latitude: stop.latitude,
-      longitude: stop.longitude
-    }
-    
-    output = {
-      stop: stopOutput
-    }
+    raise Sinatra::NotFound
+    return
   end
   
-  output.to_json
+  stopOutput = {
+    id: stop.id,
+    name: stop.name,
+    latitude: stop.latitude,
+    longitude: stop.longitude
+  }
+  
+  stopOutput.to_json
 end
 
 get '/stops/:stop_id/times' do
   content_type :json
   
   stop = Stop.find(params[:stop_id])
-  
   if stop.nil?
-    search_error = {
-      message: "Unable to find stop with id #{params[:stop_id]}"
-    }
-    output = {
-      error: search_error
-    }
-  else
-    times = StopTime.where(stop_id: params[:stop_id])
-    
-    timesOutput = Array.new
-    
-    times.each do |time|
-      timesOutput << {
-        stop_id: time.stop_id,
-        trip_id: time.trip_id,
-        route_id: time.route_id,
-        arrival_time: time.arrival_time,
-        departure_time: time.departure_time,
-        stop_sequence: time.stop_sequence
-      }
-    end
-    
-    output = {
-      stop_times: timesOutput
+    raise Sinatra::NotFound
+    return
+  end
+  
+  times = StopTime.where(stop_id: params[:stop_id])
+  timesOutput = Array.new
+  
+  times.each do |time|
+    timesOutput << {
+      stop_id: time.stop_id,
+      trip_id: time.trip_id,
+      route_id: time.route_id,
+      arrival_time: time.arrival_time,
+      departure_time: time.departure_time,
+      stop_sequence: time.stop_sequence
     }
   end
   
-  output.to_json
+  timesOutput.to_json
 end
 
 get '/stops/:stop_id/times/:route_id' do
@@ -92,43 +70,25 @@ get '/stops/:stop_id/times/:route_id' do
   stop = Stop.find(params[:stop_id])
   
   if stop.nil?
-    search_error = {
-      message: "Unable to find stop with id #{params[:stop_id]}"
-    }
-    output = {
-      error: search_error
-    }
-  else
-    times = StopTime.where(stop_id: params[:stop_id], route_id: params[:route_id])
-    
-    if times.nil?
-      search_error = {
-        message: "Route #{params[:route_id]} does not stop at stop #{params[:stop_id]}"
-      }
-      output = {
-        error: search_error
-      }
-    else
-      timesOutput = Array.new
-
-      times.each do |time|
-        timesOutput << {
-          stop_id: time.stop_id,
-          trip_id: time.trip_id,
-          route_id: time.route_id,
-          arrival_time: time.arrival_time,
-          departure_time: time.departure_time,
-          stop_sequence: time.stop_sequence
-        }
-      end
-      
-      output = {
-        stop_times: timesOutput
-      }
-    end
+    raise Sinatra::NotFound
+    return
   end
   
-  output.to_json
+  times = StopTime.where(stop_id: params[:stop_id], route_id: params[:route_id])
+  timesOutput = Array.new
+
+  times.each do |time|
+    timesOutput << {
+      stop_id: time.stop_id,
+      trip_id: time.trip_id,
+      route_id: time.route_id,
+      arrival_time: time.arrival_time,
+      departure_time: time.departure_time,
+      stop_sequence: time.stop_sequence
+    }
+  end
+  
+  timesOutput.to_json
 end
 
 get '/routes' do
@@ -143,11 +103,7 @@ get '/routes' do
     }
   end
   
-  output = {
-    routes: routes
-  }
-  
-  output.to_json
+  routes.to_json
 end
 
 get '/routes/:route_id' do
@@ -156,24 +112,16 @@ get '/routes/:route_id' do
   route = Route.find(params[:route_id])
   
   if route.nil?
-    search_error = {
-      message: "Unable to find route with id #{params[:route_id]}"
-    }
-    output = {
-      error: search_error
-    }
-  else
-    routeOutput = {
-      id: route.id,
-      name: route.name
-    }
-    
-    output = {
-      route: routeOutput
-    }
+    raise Sinatra::NotFound
+    return
   end
   
-  output.to_json
+  routeOutput = {
+    id: route.id,
+    name: route.name
+  }
+  
+  routeOutput.to_json
 end
 
 get '/routes/:route_id/times' do
@@ -182,41 +130,23 @@ get '/routes/:route_id/times' do
   route = Route.find(params[:route_id])
   
   if route.nil?
-    search_error = {
-      message: "Unable to find route with id #{params[:route_id]}"
-    }
-    output = {
-      error: search_error
-    }
-  else
-    times = StopTime.where(route_id: params[:route_id]).asc(:trip_id)
-    
-    if times.nil?
-      search_error = {
-        message: "Route #{params[:route_id]} has no stops"
-      }
-      output = {
-        error: search_error
-      }
-    else
-      timesOutput = Array.new
-
-      times.each do |time|
-        timesOutput << {
-          stop_id: time.stop_id,
-          trip_id: time.trip_id,
-          route_id: time.route_id,
-          arrival_time: time.arrival_time,
-          departure_time: time.departure_time,
-          stop_sequence: time.stop_sequence
-        }
-      end
-      
-      output = {
-        stop_times: timesOutput
-      }
-    end
+    raise Sinatra::NotFound
+    return
   end
   
-  output.to_json
+  times = StopTime.where(route_id: params[:route_id]).asc(:trip_id)
+  timesOutput = Array.new
+
+  times.each do |time|
+    timesOutput << {
+      stop_id: time.stop_id,
+      trip_id: time.trip_id,
+      route_id: time.route_id,
+      arrival_time: time.arrival_time,
+      departure_time: time.departure_time,
+      stop_sequence: time.stop_sequence
+    }
+  end
+  
+  timesOutput.to_json
 end
